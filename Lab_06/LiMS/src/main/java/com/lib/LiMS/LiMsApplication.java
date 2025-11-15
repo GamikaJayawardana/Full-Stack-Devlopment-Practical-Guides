d84938e9da0975fb138cb57fd3211032798f2b93
Gamika Jayawardana <gamikakj@gmail.com>
2025-11-15 15:12:00 +0530

    Initialize Spring Boot project for LiMS
    Add initial Spring Boot project structure for the LiMS application, including Maven configuration, application entry point, test class, and basic project files. Includes support for MongoDB, Lombok, and web dependencies.

---
+++ Lab_06/LiMS/src/main/java/com/lib/LiMS/LiMsApplication.java
@@ -0,0 +1,13 @@
+package com.lib.LiMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiMsApplication.class, args);
+	}
+
+}
